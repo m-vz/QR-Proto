43d9fc20ed44a55a2b4d5e4b53ec9e13eae91003
A <aeneas.meier@gmail.com>
2018-05-10 13:09:03 +0200

    qr phone compression

--- src/audio/QRPhone.java
+++ src/audio/QRPhone.java
@@ -6,6 +6,8 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
 import java.util.LinkedList;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
@@ -21,24 +23,22 @@ import qr_proto.QRProto;
 
 public class QRPhone {
 
-  private final long RECORDING_TIME = 500;
   private AudioFormat format;
-  private DataLine.Info info;
   private TargetDataLine line;
   private byte buffer[];
   private QRProto qrProto;
   private LinkedList<String> recordedMessages;
+  private DecimalFormat formatter;
 
   public QRPhone(QRProto proto) {
     this(4000 /*4000 minimum*/, 8, 1, true, true, proto);
   }
 
-  public QRPhone(float sampleRate, int sampleSizeInBits, int channels, boolean signed,
+  private QRPhone(float sampleRate, int sampleSizeInBits, int channels, boolean signed,
       boolean bigEndian, QRProto qrProto) {
-    format = new AudioFormat(sampleRate,
-        sampleSizeInBits, channels, signed, bigEndian);
+    format = new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
     try {
-      info = new Info(TargetDataLine.class, format);
+      Info info = new Info(TargetDataLine.class, format);
       line = (TargetDataLine) AudioSystem.getLine(info); //~= input stream
       line.open(format);
       line.start();
@@ -49,7 +49,8 @@ public class QRPhone {
 
     this.qrProto = qrProto;
     this.recordedMessages = new LinkedList<>();
-    }
+    this.formatter = new DecimalFormat("000000");
+  }
 
   public ByteArrayOutputStream recordAudio(long recordingTime) {
     long startTime = System.currentTimeMillis();
@@ -97,38 +98,72 @@ public class QRPhone {
     }
   }
 
+
   public String convertAudioToString(ByteArrayOutputStream stream) {
     byte[] bytes = stream.toByteArray();
-    return new String(toCharArray(bytes));
+    int inputlength = bytes.length;
+    StringBuilder output = new StringBuilder();
+
+    if (inputlength < 1000000) {
+      output.append(formatter.format(inputlength));
+    } else {
+      throw new InputMismatchException(
+          "Audio-Input longer than 999999 symbols. Might cause compression errors");//TODO: use Base64 for compression
+    }
+
+    for (int i = 0; i < inputlength; i++) {
+      char byteToHandle = (char) (bytes[i] + 128);
+      if (byteToHandle == '\u0080') {
+        long charsToSkip = 1;
+        while ((char) (bytes[i++] + 128) == '\u0080' && i < inputlength) {
+          charsToSkip++;
+        }
+        if (charsToSkip <= 6) {
+          for (int j = 0; j < charsToSkip; j++) {
+            output.append('\u0080');
+          }
+        } else {
+          output.append('\\');
+          output.append(formatter.format(charsToSkip));
+        }
+      } else if (byteToHandle == '\\') {
+        output.append("\\\\");
+      } else {
+        output.append(byteToHandle);
+      }
+    }
+    Log.outln("Compressed message " + inputlength / output.length() + " fold.");
+    if (inputlength / output.length() > 600) {
+      Log.errln(output.toString());
+    }
+    return output.toString();
   }
 
   public ByteArrayOutputStream convertStringToByteArrayOutputStream(String input) {
-    byte[] output = toByteArray(input.toCharArray());
+
+    int outputLength = Integer.parseInt(input.substring(0, 6));
+    byte[] output = new byte[outputLength];
+    for (int i = 6; i < outputLength; i++) {
+      char insert = input.charAt(i);
+      if (insert == '\\') {
+        i++;
+        if (input.charAt(i) == '\\') {
+          output[i] = (byte) (insert);
+          output[i] -= 128;
+        } else {
+          for (int j = 0; j < Integer.parseInt(input.substring(i, i + 6)); j++) {
+            output[i] = 0;
+          }
+          i += 6;
+        }
+      }
+      output[i] = (byte) (insert);
+      output[i] -= 128;
+    }
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream(output.length);
     outputStream.write(output, 0, output.length);
 
     return outputStream;
-  }
-
-  char[] toCharArray(byte[] bytes) {
-    int length = bytes.length;
-    char[] chars = new char[length];
-
-    for (int i = 0; i < length; i++) {
-      chars[i] = (char) (bytes[i] + 128);
-    }
-
-    return chars;
-  }
-
-  byte[] toByteArray(char[] chars) {
-    int length = chars.length;
-    byte[] bytes = new byte[length];
-    for (int i = 0; i < length; i++) {
-      bytes[i] = (byte) (chars[i]);
-      bytes[i] -= 128;
-    }
-    return bytes;
   }
 
 
@@ -137,6 +172,7 @@ public class QRPhone {
     @Override
     public void run() {
       Log.outln("Thread started");
+      long RECORDING_TIME = 2000;
       qrProto.sendMessage(convertAudioToString(recordAudio(RECORDING_TIME)));
       while (true) {
         recordedMessages.add(convertAudioToString(recordAudio(RECORDING_TIME)));
