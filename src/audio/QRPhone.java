6fa85b8ca252a9019132d2997647d1dc3049d6df
A <aeneas.meier@gmail.com>
2018-05-14 12:23:41 +0200

    compression on message-level removed compression of qr phone

--- src/audio/QRPhone.java
+++ src/audio/QRPhone.java
@@ -7,9 +7,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.DecimalFormat;
 import java.util.LinkedList;
-import java.util.zip.DataFormatException;
-import java.util.zip.Deflater;
-import java.util.zip.Inflater;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -104,52 +101,30 @@ public class QRPhone {
     }
   }
 
-  public ByteArrayOutputStream inflate (String inputString){
+  public ByteArrayOutputStream StringtoByteArrayOutputStream(String inputString){
 
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    int outputLength = Integer.parseInt(inputString.substring(0,6));
-    Inflater decompressor = new Inflater();
     char[] charInput = inputString.toCharArray();
-    byte[] byteInput = new byte[charInput.length-6];
+    byte[] byteInput = new byte[charInput.length];
 
     for (int i=0; i < byteInput.length; i++){
       byteInput[i] = (byte) (charInput[i+6] + 128);
     }
-    decompressor.setInput(byteInput, 0, outputLength);
-    byte[] result = new byte[outputLength];
-
-    try {
-      int resultLength = decompressor.inflate(result);
-    } catch (DataFormatException e) {
-      e.printStackTrace();
-    }
-
-    decompressor.end();
-    outputStream.write(result,0,outputLength);
+    outputStream.write(byteInput,0,byteInput.length);
 
     return outputStream;
-
   }
 
 
-  public String deflate (ByteArrayOutputStream inputStream) {
+  public String ByteArrayOutputStreamtoString (ByteArrayOutputStream inputStream) {
 
     byte[] input = inputStream.toByteArray();
-    byte[] output = new byte[input.length];
-    Deflater compressor = new Deflater();
-    compressor.setInput(input);
-    compressor.finish();
-    int compressedInputLength = compressor.deflate(output);
-    compressor.end();
 
-    char[] outputString = new char[compressedInputLength];
-    for (int i=0; i < compressedInputLength; i++){
-      outputString[i] = (char) (output[i] + 128);
+    char[] outputString = new char[input.length];
+    for (int i=0; i < input.length; i++){
+      outputString[i] = (char) (input[i] + 128);
     }
-    String result = formatter.format(compressedInputLength);
-    result += new String(outputString);
-    Log.outln("Deflate compressed message " + (float) input.length / result.length() + " fold.");
-    return result;
+    return new String(outputString);
   }
 
 
@@ -161,11 +136,11 @@ public class QRPhone {
     @Override
     public void run() {
       Log.outln("Thread started");
-      qrProto.sendMessage(deflate(recordAudio(RECORDING_TIME)));
+      qrProto.sendMessage(ByteArrayOutputStreamtoString(recordAudio(RECORDING_TIME)));
       while(!shouldStop) {
         synchronized(this) {
           if(!shouldStop)
-            recordedMessages.add(deflate(recordAudio(RECORDING_TIME)));
+            recordedMessages.add(ByteArrayOutputStreamtoString(recordAudio(RECORDING_TIME)));
 
           if(canSend && !recordedMessages.isEmpty())
             qrProto.sendMessage(recordedMessages.poll());
@@ -183,7 +158,7 @@ public class QRPhone {
       while(!shouldStop) {
         synchronized(this) {
           if(!playbackMessages.isEmpty())
-            playAudio(inflate(playbackMessages.poll()));
+            playAudio(StringtoByteArrayOutputStream(playbackMessages.poll()));
           else {
             try {
               Thread.sleep(10);
--- src/qr_proto/qr/Message.java
+++ src/qr_proto/qr/Message.java
@@ -5,6 +5,10 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.text.DecimalFormat;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 public class Message {
   public static final String MESSAGE_END = "\\0";
@@ -12,11 +16,13 @@ public class Message {
 
   private String message;
   private boolean complete, escaped;
+  private DecimalFormat formatter;
 
   public Message(String message, boolean complete, boolean escaped) {
     this.message = message;
     this.complete = complete;
     this.escaped = escaped;
+    this.formatter = new DecimalFormat("000000");
   }
 
   public String removeSubstring(int beginIndex, int endIndex) {
@@ -27,6 +33,28 @@ public class Message {
 
   public Message escape (){
     if(!escaped) {
+      byte[] toCompress;
+      byte[] output = new byte[message.length()];
+      Deflater compressor = new Deflater();
+
+      try {
+        toCompress = message.getBytes("UTF-8");
+        compressor.setInput(toCompress);
+        message = formatter.format(toCompress.length);
+      } catch (UnsupportedEncodingException e) {
+        e.printStackTrace();
+      }
+
+      compressor.finish();
+      int compressedDataLength = compressor.deflate(output);
+      compressor.end();
+
+      try {
+        message += new String(output, 0, compressedDataLength, "UTF-8");
+      } catch (UnsupportedEncodingException e) {
+        e.printStackTrace();
+      }
+
       message = message.replace("\\", "\\b");
       if(complete)
         message += MESSAGE_END;
@@ -37,20 +65,23 @@ public class Message {
 
   public Message unescape () {
     if(escaped) {
-//      if(COMPRESS) {
-//        try {
-//          InputStreamReader reader = new InputStreamReader(new BrotliInputStream(new ByteArrayInputStream(message.getBytes("UTF-8"))));
-//          StringBuilder builder = new StringBuilder();
-//          int r;
-//          while((r = reader.read()) != -1)
-//            builder.append((char) r);
-//          message = builder.toString();
-//        } catch (UnsupportedEncodingException e) {
-//          e.printStackTrace();
-//        } catch(IOException e) {
-//          e.printStackTrace();
-//        }
-//      }
+      int uncompressedDataLength = Integer.parseInt(message.substring(0,6));
+      Inflater decompresser = new Inflater();
+      decompresser.setInput(message.getBytes(), 6, message.length()-6);
+      byte[] result = new byte[uncompressedDataLength];
+      try {
+        decompresser.inflate(result);
+      } catch (DataFormatException e) {
+        e.printStackTrace();
+      }
+      decompresser.end();
+
+      try {
+        message = new String(result, 0, uncompressedDataLength, "UTF-8");
+      } catch (UnsupportedEncodingException e) {
+        e.printStackTrace();
+      }
+
       int length = getMessageLength();
       if(complete)
         length -= MESSAGE_END.length();
